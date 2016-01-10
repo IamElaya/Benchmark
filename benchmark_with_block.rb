69049b0e1a5d21ad661a99f70728cc35d74957f9
Elaya Sison <asison13@gmail.com>
2016-01-09 22:31:13 -0800

    First Commit

---
+++ benchmark_with_block.rb
@@ -0,0 +1,19 @@
+def benchmark
+  # Your benchmarking code goes here.
+  start = Time.now
+  yield
+  finish = Time.now
+
+  diff = finish - start
+end
+
+
+ 
+#Be careful, pasting this into IRB will take a long time to print.
+#It's a loooong string. :)
+long_string = "apple"*100000000
+ 
+running_time = benchmark { long_string.reverse }
+ 
+puts "string.reverse took #{running_time} seconds to run"
+
